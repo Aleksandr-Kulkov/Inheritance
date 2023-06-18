e4f8c7eaded83937c2d6ce42370fcd9d0971dd52
Aleksandr Kulkov <129850584+Aleksandr-Kulkov@users.noreply.github.com>
2023-06-18 16:09:25 +0300

    Add file cat.py

---
+++ cat.py
@@ -0,0 +1,15 @@
+# Дом питомца (http://158.160.56.133/app/pets)
+class Cat:
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self.age = age
+
+    def getName(self):
+        return self.name
+
+    def getGender(self):
+        return self.gender
+
+    def getAge(self):
+        return self.age
